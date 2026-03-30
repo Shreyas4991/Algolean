52d6c124c6367cf9e113c4e3eed2c74ccad96d34
Shreyas <shreyasss94@gmail.com>
2026-03-30 15:21:36 +0200

    Start with a proper reorg

--- Algolean.lean
+++ Algolean.lean
@@ -7,6 +7,7 @@ public import Algolean.Algorithms.ListOrderedInsert
 public import Algolean.Algorithms.MergeSort
 public import Algolean.Algorithms.VecSearch
 public import Algolean.Complexity.Basic
+public import Algolean.Complexity.PolytimeBasicClasses
 public import Algolean.LowerBounds.ComparisonSort
 public import Algolean.Models.Circuits
 public import Algolean.Models.FanInTwoCircuits
