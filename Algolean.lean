f454e18879807f22cd77fea2cc8ac24f6e06b5ff
Shreyas <shreyasss94@gmail.com>
2026-05-05 16:51:27 +0200

    Update main file

--- Algolean.lean
+++ Algolean.lean
@@ -13,6 +13,7 @@ public import Algolean.LowerBounds.ComparisonSort
 public import Algolean.Models.Circuits
 public import Algolean.Models.Comparison
 public import Algolean.Models.FanInTwoCircuits
+public import Algolean.Models.GraphModels
 public import Algolean.Models.ListComparisonSearch
 public import Algolean.Models.ListComparisonSort
 public import Algolean.Models.RandomSample
@@ -20,4 +21,5 @@ public import Algolean.Models.ReadOnlyVec
 public import Algolean.Models.ReadWriteVec
 public import Algolean.Models.RobertsonWebb
 public import Algolean.Models.SingleTapeTM
+public import Algolean.QueryComposition
 public import Algolean.QueryModel
